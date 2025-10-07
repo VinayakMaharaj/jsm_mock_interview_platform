5fa5291a4ab33caef0a85aae5cebcd55bb647e79
Vinayak <vinayakcpa@gmail.com>
2025-10-06 21:59:38 -0400

    did starter root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,17 @@
 import { ReactNode } from 'react'
+import Link from "next/link";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div>{children}</div>
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className="flex items-center gap-2">
+                <Image src="/logo.svg" alt="Logo" width={38} height={38} />
+
+
+                </Link>
+            </nav>
+        </div>
     )
 }
 export default RootLayout

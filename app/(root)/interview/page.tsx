61d07b39ad9076a4e52c151149dae1f6a3634a2c
Vinayak <vinayakcpa@gmail.com>
2025-10-07 21:14:29 -0400

    added code for interview page

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Page = () => {
+const Page = async () => {
+    const user = await getCurrentUser();
+
     return (
-        <div>Interview Page</div>
-    )
-}
-export default Page
+        <>
+            <h3>Interview generation</h3>
+
+            <Agent
+                userName={user?.name!}
+                userId={user?.id}
+                profileImage={user?.profileURL}
+                type="generate"
+            />
+        </>
+    );
+};
+
+export default Page;
